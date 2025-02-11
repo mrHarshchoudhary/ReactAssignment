1fa00c92f051a5b5b53d2afe957bbcb328ba1eb0
mrHarshchoudhary <Harshchoudharyv@gmail.com>
2025-02-11 11:39:58 +0530

    assignment-2

---
+++ Assignment-2/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Greet from './Component/Greet'
+
+
+const App = () => {
+  return (
+    <>
+   <Greet/>
+    </>
+  )
+}
+
+export default App
---
+++ Assignment-2/src/Component/Greet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Greet = () => {
+  return (
+   <>
+   <h1>Hello everyone this is Assignment-2 of react js</h1>
+   </>
+  )
+}
+
+export default Greet
